080fe4f84e85c0742edc54d96b215a42889dece2
Mehran Ghandehari <mehran.ghandehari@colorado.edu>
2018-07-26 11:56:51 -0600

    Update and rename borjhaie hanoi.cpp to HanoiTower.cpp

--- Code/HanoiTower.cpp
+++ Code/HanoiTower.cpp
@@ -1,4 +1,5 @@
-//Error	5	fatal error C1010: unexpected end of file while looking for precompiled header. Did you forget to add '#include "stdafx.h"' to your source?	c:\users\kps\desktop\c++\test1\test1\test1\test1.cpp	50	test1
+//Mehran Ghandehari
+//HanoiTower
 #include "stdafx.h"
 #include <iostream>
 #include<conio.h>
